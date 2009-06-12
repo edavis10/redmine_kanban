18c169d136bd855c9f0006ae26ce34db94635649
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-12 16:36:44 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_kanban do
+  name 'Redmine Kanban plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
