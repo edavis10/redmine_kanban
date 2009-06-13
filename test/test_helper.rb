f81d7c972951cc6b300af168f51064f1013465d7
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-12 17:39:09 -0700

    [#2594] Removed unneeded Rake test tasks and added a basic sanity test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
+
+Rails::Initializer.run do |config|
+  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
+end
---
+++ test/unit/sanity_test.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SanityTest < Test::Unit::TestCase
+  def test_is_sane
+    assert true
+  end
+
+  should "be true" do
+    assert true
+  end
+end
