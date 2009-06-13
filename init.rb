2bc4a781fe36072570b07cc07c1da10133733339
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-12 17:11:37 -0700

    [#2594] Added a Readme and configured the init.rb

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_kanban do
-  name 'Redmine Kanban plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Kanban'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'The Redmine Kanban plugin is used to manage issues according to the Kanban system of project management.'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
 end
