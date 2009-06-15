fcf53d204c70e450ef19ca3d8b9e0b776aa76eae
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-15 16:27:03 -0700

    [#2597] Refactored the controller tests

--- test/functional/kanbans_controller_test.rb
+++ test/functional/kanbans_controller_test.rb
@@ -4,98 +4,117 @@ class KanbansControllerTest < ActionController::TestCase
   context "on GET to :show" do
     setup {
       configure_plugin
-      # Private Project
-      private_project = make_project_with_trackers(:is_public => false)
-      5.times do
-        Issue.make(:tracker => private_project.trackers.first,
-                   :project => private_project,
-                   :status => IssueStatus.find_by_name('Active'))
-
-      end
-
-      public_project = make_project_with_trackers(:is_public => true)
-      6.times do
-        Issue.make(:tracker => public_project.trackers.first,
-                   :project => public_project,
-                   :status => IssueStatus.find_by_name('New'))
-      end
-
-      high_priority = IssuePriority.make(:name => "High")
-      5.times do
-        Issue.make(:tracker => public_project.trackers.first,
-                   :project => public_project,
-                   :priority => high_priority,
-                   :status => IssueStatus.find_by_name('Unstaffed'))
-      end
-
-      medium_priority = IssuePriority.make(:name => "Medium")
-      7.times do
-        Issue.make(:tracker => public_project.trackers.first,
-                   :project => public_project,
-                   :priority => medium_priority,
-                   :status => IssueStatus.find_by_name('Unstaffed'))
-      end
-
-      low_priority = IssuePriority.make(:name => "Low")
-      5.times do
-        Issue.make(:tracker => public_project.trackers.first,
-                   :project => public_project,
-                   :priority => low_priority,
-                   :status => IssueStatus.find_by_name('Unstaffed'))
-      end
-
+      @private_project = make_project_with_trackers(:is_public => false)
+      @public_project = make_project_with_trackers(:is_public => true)
       @user = User.make
       @request.session[:user_id] = @user.id
-      get :show
     }
 
-    should_assign_to :settings
-    should_assign_to :incoming_issues
-    should_assign_to :backlog_issues
+    # TODO: find a way to setup data before the other contexts before
+    # calling `get`
+    context "" do
+      setup {
+        get :show
+      }
+      
+      should_assign_to :settings
+      should_assign_to :incoming_issues
+      should_assign_to :backlog_issues
 
-    should_respond_with :success
-    should_render_template :show
-    should_not_set_the_flash
-
-    should "only get incoming issues up to the limit" do
-      assert_equal 5, assigns(:incoming_issues).size
+      should_respond_with :success
+      should_render_template :show
+      should_not_set_the_flash
     end
+    
+    context "for incoming issues" do
+      setup do
+        5.times do
+          Issue.make(:tracker => @private_project.trackers.first,
+                     :project => @private_project,
+                     :status => IssueStatus.find_by_name('New'))
 
-    should "only get incoming issues with the configured status" do
-      assigns(:incoming_issues).each do |issue|
-        assert_equal 'New', issue.status.name
+        end
+
+        6.times do
+          Issue.make(:tracker => @public_project.trackers.first,
+                     :project => @public_project,
+                     :status => IssueStatus.find_by_name('New'))
+        end
+
+        get :show
+      end
+      
+      should "only get incoming issues up to the limit" do
+        assert_equal 5, assigns(:incoming_issues).size
+      end
+
+      should "only get incoming issues with the configured status" do
+        assigns(:incoming_issues).each do |issue|
+          assert_equal 'New', issue.status.name
+        end
       end
     end
 
-    should "only get backlog issues up to the limit" do
-      assert_equal 3, assigns(:backlog_issues).size # Priorities
-      assert_equal 15, assigns(:backlog_issues).values.collect.flatten.size # Issues
-    end
+    context "for backlog issues" do
+      setup do
+        high_priority = IssuePriority.make(:name => "High")
+        5.times do
+          Issue.make(:tracker => @public_project.trackers.first,
+                     :project => @public_project,
+                     :priority => high_priority,
+                     :status => IssueStatus.find_by_name('Unstaffed'))
+        end
 
-    should "only get backlog issues with the configured status" do
-      assigns(:backlog_issues).each do |priority, issues|
-        issues.each do |issue|
-          assert_equal 'Unstaffed', issue.status.name
+        medium_priority = IssuePriority.make(:name => "Medium")
+        7.times do
+          Issue.make(:tracker => @public_project.trackers.first,
+                     :project => @public_project,
+                     :priority => medium_priority,
+                     :status => IssueStatus.find_by_name('Unstaffed'))
+        end
+
+        low_priority = IssuePriority.make(:name => "Low")
+        5.times do
+          Issue.make(:tracker => @public_project.trackers.first,
+                     :project => @public_project,
+                     :priority => low_priority,
+                     :status => IssueStatus.find_by_name('Unstaffed'))
+        end
+
+        get :show
+
+      end
+      
+      should "only get backlog issues up to the limit" do
+        assert_equal 3, assigns(:backlog_issues).size # Priorities
+        assert_equal 15, assigns(:backlog_issues).values.collect.flatten.size # Issues
+      end
+
+      should "only get backlog issues with the configured status" do
+        assigns(:backlog_issues).each do |priority, issues|
+          issues.each do |issue|
+            assert_equal 'Unstaffed', issue.status.name
+          end
         end
       end
-    end
 
-    should "group backlog issues by IssuePriority" do
-      assert_equal IssuePriority.find_by_name("High"),  assigns(:backlog_issues).keys[0]
-      assert_equal IssuePriority.find_by_name("Medium"),  assigns(:backlog_issues).keys[1]
-      assert_equal IssuePriority.find_by_name("Low"),  assigns(:backlog_issues).keys[2]
-    end
+      should "group backlog issues by IssuePriority" do
+        assert_equal IssuePriority.find_by_name("High"),  assigns(:backlog_issues).keys[0]
+        assert_equal IssuePriority.find_by_name("Medium"),  assigns(:backlog_issues).keys[1]
+        assert_equal IssuePriority.find_by_name("Low"),  assigns(:backlog_issues).keys[2]
+      end
 
-  should "render nested lists for the grouping" do
-      assert_select("ol#backlog-issues") do
-        assert_select("ol.high") do
-          assert_select("li", :count => 5)
-        end
-        assert_select("ol.medium") do
-          assert_select("li", :count => 7)
-        end
-        assert_select("ol.low") do
-          assert_select("li", :count => 3)
+      should "render nested lists for the grouping" do
+        assert_select("ol#backlog-issues") do
+          assert_select("ol.high") do
+            assert_select("li", :count => 5)
+          end
+          assert_select("ol.medium") do
+            assert_select("li", :count => 7)
+          end
+          assert_select("ol.low") do
+            assert_select("li", :count => 3)
+          end
         end
       end
     end
