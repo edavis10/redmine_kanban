8d532852001106ddeab65bfa5df9396a1eb08a8f
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-16 14:30:01 -0700

    [#2597] Hooked up the drag and drop between the Incoming and Backlog panes.

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -1,1 +1,22 @@
+jQuery(function($) {
+    $("#ajax-indicator").ajaxStart(function(){ $(this).show();  });
+    $("#ajax-indicator").ajaxStop(function(){ $(this).hide();  });
+
+
+    $("#incoming-issues").sortable({
+        cancel: 'a',
+        connectWith: ['#backlog-issues'],
+        placeholder: 'drop-accepted',
+        dropOnEmpty: true
+    });
+
+    $("#backlog-issues").sortable({
+        cancel: 'a',
+        connectWith: ['#incoming-issues'],
+        items: 'li.issue',
+        placeholder: 'drop-accepted',
+        dropOnEmpty: true
+    });
+
+});
 
