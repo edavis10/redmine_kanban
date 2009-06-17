0317e9dc9b63e0d0b39939ec9a0e65305627381a
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-16 17:48:05 -0700

    [#2597] Hooked up the drag and drop from Incoming to backlog.

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -15,8 +15,30 @@ jQuery(function($) {
         connectWith: ['#incoming-issues'],
         items: 'li.issue',
         placeholder: 'drop-accepted',
-        dropOnEmpty: true
+        dropOnEmpty: true,
+        update: function (event, ui) {
+            updatePanes(ui.item,ui.sender,$(this));
+        }
     });
+
+    updatePanes = function(issue, from, to) {
+        var issue_id = issue.attr('id').split('_')[1];
+        var from_pane = from.attr('id').split('-')[0];
+        var to_pane = to.attr('id').split('-')[0];
+
+        $.ajax({
+            type: "PUT",
+            url: 'kanban.js',
+            data: 'issue_id=' + issue_id + '&from=' + from_pane + '&to=' + to_pane,
+            success: function(response) {
+                var partials = $.secureEvalJSON(response);
+                $(from).parent().html(partials.from);
+                $(to).parent().html(partials.to);
+                // TODO: reattach sortables
+            }
+        });
+
+    };
 
 });
 
