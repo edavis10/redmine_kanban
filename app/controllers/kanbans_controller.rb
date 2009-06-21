d871740781af0f3c9fba2e0edba721f9ef9805b9
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-21 14:49:48 -0700

    [#2621] Moved all the finder methods from the Controller into a new Kanban model.

--- app/controllers/kanbans_controller.rb
+++ app/controllers/kanbans_controller.rb
@@ -5,9 +5,10 @@ class KanbansController < ApplicationController
   
   def show
     @settings = Setting.plugin_redmine_kanban
-    @incoming_issues = get_incoming_issues
-    @quick_issues = get_quick_issues
-    @backlog_issues = get_backlog_issues(@quick_issues.values.flatten.collect(&:id))
+    @kanban = Kanban.find
+    @incoming_issues = @kanban.incoming_issues
+    @quick_issues = @kanban.quick_issues
+    @backlog_issues = @kanban.backlog_issues
   end
 
   def update
@@ -16,8 +17,10 @@ class KanbansController < ApplicationController
     @to = params[:to]
     saved = change_issue_status(params[:issue_id], params[:from], params[:to], User.current)
 
-    @incoming_issues = get_incoming_issues
-    @backlog_issues = get_backlog_issues
+    @kanban = Kanban.find
+    @incoming_issues = @kanban.incoming_issues
+    @quick_issues = @kanban.quick_issues
+    @backlog_issues = @kanban.backlog_issues
     respond_to do |format|
 
       if saved
@@ -44,40 +47,6 @@ class KanbansController < ApplicationController
   end
   
   private
-  def get_incoming_issues
-    return Issue.visible.find(:all,
-                              :limit => @settings['panes']['incoming']['limit'],
-                              :order => "#{Issue.table_name}.created_on ASC",
-                              :conditions => {:status_id => @settings['panes']['incoming']['status']})
-  end
-
-  def get_backlog_issues(exclude_ids=[])
-    issues = Issue.visible.all(:limit => @settings['panes']['backlog']['limit'],
-                               :order => "#{Issue.table_name}.created_on ASC",
-                               :include => :priority,
-                               :conditions => ["#{Issue.table_name}.status_id IN (?) AND #{Issue.table_name}.id NOT IN (?)", @settings['panes']['backlog']['status'], exclude_ids])
-
-    return issues.group_by {|issue|
-      issue.priority
-    }.sort {|a,b|
-      a[0].position <=> b[0].position # Sorted based on IssuePriority#position
-    }
-  end
-
-  # TODO: similar to backlog issues
-  def get_quick_issues
-    issues = Issue.visible.all(:limit => @settings['panes']['quick-tasks']['limit'],
-                               :order => "#{Issue.table_name}.created_on ASC",
-                               :include => :priority,
-                               :conditions => {:status_id => @settings['panes']['backlog']['status'], :estimated_hours => nil})
-
-    return issues.group_by {|issue|
-      issue.priority
-    }.sort {|a,b|
-      a[0].position <=> b[0].position # Sorted based on IssuePriority#position
-    }
-  end
-
   def change_issue_status(issue, from, to, user)
     issue = Issue.find_by_id(issue)
 
---
+++ app/models/kanban.rb
@@ -0,0 +1,52 @@
+class Kanban
+  attr_accessor :incoming_issues
+  attr_accessor :quick_issues
+  attr_accessor :backlog_issues
+  attr_accessor :settings
+  
+  def self.find
+    kanban = Kanban.new
+    kanban.settings = Setting.plugin_redmine_kanban
+    kanban.incoming_issues = kanban.get_incoming_issues
+    kanban.quick_issues = kanban.get_quick_issues
+    kanban.backlog_issues = kanban.get_backlog_issues(kanban.quick_issues.values.flatten.collect(&:id))
+    kanban
+  end
+
+  def get_incoming_issues
+    return Issue.visible.find(:all,
+                              :limit => @settings['panes']['incoming']['limit'],
+                              :order => "#{Issue.table_name}.created_on ASC",
+                              :conditions => {:status_id => @settings['panes']['incoming']['status']})
+  end
+
+  def get_backlog_issues(exclude_ids=[])
+    issues = Issue.visible.all(:limit => @settings['panes']['backlog']['limit'],
+                               :order => "#{Issue.table_name}.created_on ASC",
+                               :include => :priority,
+                               :conditions => ["#{Issue.table_name}.status_id IN (?) AND #{Issue.table_name}.id NOT IN (?)", @settings['panes']['backlog']['status'], exclude_ids])
+
+    return issues.group_by {|issue|
+      issue.priority
+    }.sort {|a,b|
+      a[0].position <=> b[0].position # Sorted based on IssuePriority#position
+    }
+  end
+
+  # TODO: similar to backlog issues
+  def get_quick_issues
+    issues = Issue.visible.all(:limit => @settings['panes']['quick-tasks']['limit'],
+                               :order => "#{Issue.table_name}.created_on ASC",
+                               :include => :priority,
+                               :conditions => {:status_id => @settings['panes']['backlog']['status'], :estimated_hours => nil})
+
+    return issues.group_by {|issue|
+      issue.priority
+    }.sort {|a,b|
+      a[0].position <=> b[0].position # Sorted based on IssuePriority#position
+    }
+  end
+
+
+  
+end
