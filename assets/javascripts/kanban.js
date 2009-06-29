7498cc9780c52606a95c5dd75eaa02acddfcaf55
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-29 14:14:49 -0700

    Reformatted kanban.js using js2-mode.  No code changes

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -1,103 +1,103 @@
 jQuery(function($) {
-    $("#ajax-indicator").ajaxStart(function(){ $(this).show();  });
-    $("#ajax-indicator").ajaxStop(function(){ $(this).hide();  });
+  $("#ajax-indicator").ajaxStart(function(){ $(this).show();  });
+  $("#ajax-indicator").ajaxStop(function(){ $(this).hide();  });
 
-    attachSortables = function() {
-        $("#incoming-issues").sortable({
-            cancel: 'a',
-            connectWith: ['#backlog-issues'],
-            placeholder: 'drop-accepted',
-            dropOnEmpty: true
-        });
+  attachSortables = function() {
+    $("#incoming-issues").sortable({
+      cancel: 'a',
+      connectWith: ['#backlog-issues'],
+      placeholder: 'drop-accepted',
+      dropOnEmpty: true
+    });
 
-        $("#backlog-issues").sortable({
-            cancel: 'a',
-            connectWith: ['#incoming-issues','#selected-issues', '.active-issues'],
-            items: 'li.issue',
-            placeholder: 'drop-accepted',
-            dropOnEmpty: true,
-            receive: function (event, ui) {
-                updatePanes(ui.item,ui.sender,$(this));
-            }
-        });
-
-        $("#selected-issues").sortable({
-            cancel: 'a',
-            connectWith: ['#backlog-issues'],
-            items: 'li.issue',
-            placeholder: 'drop-accepted',
-            dropOnEmpty: true,
-            receive: function (event, ui) {
-                updatePanes(ui.item,ui.sender,$(this));
-            },
-            update: function (event, ui) {
-              // Allow drag and drop inside the list
-              if (ui.sender == null && event.target == this) {
-                updatePanes(ui.item,ui.sender,$(this));
-              }
-            }
-        });
-
-        $(".active-issues").sortable({
-            cancel: 'a',
-            connectWith: ['#backlog-issues'],
-            items: 'li.issue',
-            placeholder: 'drop-accepted',
-            dropOnEmpty: true,
-            receive: function (event, ui) {
-                updatePanes(ui.item,ui.sender,$(this));
-            },
-            update: function (event, ui) {
-              // Allow drag and drop inside the list
-              if (ui.sender == null && event.target == this) {
-                updatePanes(ui.item,ui.sender,$(this));
-              }
-            }
-        });
-
-    },
-
-    attachSortables();
-
-    updatePanes = function(issue, from, to) {
-          var issue_id = issue.attr('id').split('_')[1];
-        var to_pane = to.attr('id').split('-')[0];
-        var to_order = to.sortable('serialize', {'key': 'to_issue[]'});
-
-      if (from) {
-        var from_pane = from.attr('id').split('-')[0];
-        var from_order = from.sortable('serialize', {'key': 'from_issue[]'});
-      } else {
-        var from_pane = '';
-        var from_order = [];
+    $("#backlog-issues").sortable({
+      cancel: 'a',
+      connectWith: ['#incoming-issues','#selected-issues', '.active-issues'],
+      items: 'li.issue',
+      placeholder: 'drop-accepted',
+      dropOnEmpty: true,
+      receive: function (event, ui) {
+        updatePanes(ui.item,ui.sender,$(this));
       }
+    });
 
-      // Active pane needs to send which user was modified
-      if (to_pane == 'active') {
-        var user_id = to.attr('id').split('-')[3];
-      } else if (from_pane == 'active'){
-        var user_id = from.attr('id').split('-')[3];
-      } else {
-        var user_id = null;
+    $("#selected-issues").sortable({
+      cancel: 'a',
+      connectWith: ['#backlog-issues'],
+      items: 'li.issue',
+      placeholder: 'drop-accepted',
+      dropOnEmpty: true,
+      receive: function (event, ui) {
+        updatePanes(ui.item,ui.sender,$(this));
+      },
+      update: function (event, ui) {
+        // Allow drag and drop inside the list
+        if (ui.sender == null && event.target == this) {
+          updatePanes(ui.item,ui.sender,$(this));
+        }
       }
+    });
+
+    $(".active-issues").sortable({
+      cancel: 'a',
+      connectWith: ['#backlog-issues'],
+      items: 'li.issue',
+      placeholder: 'drop-accepted',
+      dropOnEmpty: true,
+      receive: function (event, ui) {
+        updatePanes(ui.item,ui.sender,$(this));
+      },
+      update: function (event, ui) {
+        // Allow drag and drop inside the list
+        if (ui.sender == null && event.target == this) {
+          updatePanes(ui.item,ui.sender,$(this));
+        }
+      }
+    });
+
+  },
+
+  attachSortables();
+
+  updatePanes = function(issue, from, to) {
+    var issue_id = issue.attr('id').split('_')[1];
+    var to_pane = to.attr('id').split('-')[0];
+    var to_order = to.sortable('serialize', {'key': 'to_issue[]'});
+
+    if (from) {
+      var from_pane = from.attr('id').split('-')[0];
+      var from_order = from.sortable('serialize', {'key': 'from_issue[]'});
+    } else {
+      var from_pane = '';
+      var from_order = [];
+    }
+
+    // Active pane needs to send which user was modified
+    if (to_pane == 'active') {
+      var user_id = to.attr('id').split('-')[3];
+    } else if (from_pane == 'active'){
+      var user_id = from.attr('id').split('-')[3];
+    } else {
+      var user_id = null;
+    }
 
 
-        $.ajax({
-            type: "PUT",
-            url: 'kanban.js',
-            data: 'issue_id=' + issue_id + '&from=' + from_pane + '&to=' + to_pane + '&' + from_order + '&' + to_order + '&user_id=' + user_id,
-            success: function(response) {
-                var partials = $.secureEvalJSON(response);
-                $(from).parent().html(partials.from);
-                $(to).parent().html(partials.to);
+    $.ajax({
+      type: "PUT",
+      url: 'kanban.js',
+      data: 'issue_id=' + issue_id + '&from=' + from_pane + '&to=' + to_pane + '&' + from_order + '&' + to_order + '&user_id=' + user_id,
+      success: function(response) {
+        var partials = $.secureEvalJSON(response);
+        $(from).parent().html(partials.from);
+        $(to).parent().html(partials.to);
 
-                attachSortables();
+        attachSortables();
 
-            },
-            error: function(response) {
-                $("div.error").html("Error saving lists.  Please refresh the page and try again.").show();
-            }
-        });
-    };
+      },
+      error: function(response) {
+        $("div.error").html("Error saving lists.  Please refresh the page and try again.").show();
+      }
+    });
+  };
 });
 
