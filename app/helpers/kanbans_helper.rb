00c32c62e90a47efa17548773bfd4152077da660
Eric Davis <edavis@littlestreamsoftware.com>
2009-06-29 15:39:48 -0700

    [#2604] Added logic to prevent uses from pushing tasks on other people

--- app/helpers/kanbans_helper.rb
+++ app/helpers/kanbans_helper.rb
@@ -10,4 +10,11 @@ module KanbansHelper
       ''
     end
   end
+
+  # Returns the CSS class for jQuery to hook into.  Current users are
+  # allowed to Drag and Drop items into their own list, but not other
+  # people's lists
+  def allowed_to_assign_staffed_issue_to(user)
+    User.current == user ? 'allowed' : ''
+  end
 end
