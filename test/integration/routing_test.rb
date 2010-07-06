8f8cb714b082ecf268e9afe9b053f458ddb0ebfd
Eric Davis <edavis@littlestreamsoftware.com>
2010-07-06 12:13:52 -0700

    [#3916] Added test for the existing routing.

---
+++ test/integration/routing_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class RoutingTest < ActionController::IntegrationTest
+  should_route :get, "/kanban", :controller => 'kanbans', :action => 'show'
+  should_route :put, "/kanban", :controller => 'kanbans', :action => 'update'
+  should_route :put, "/kanban.js", :controller => 'kanbans', :action => 'update', :format => 'js'
+  should_route :put, "/kanban/sync", :controller => 'kanbans', :action => 'sync'
+
+  should_route :get, "/kanban_issues/100/edit.js", :controller => 'kanban_issues', :action => 'edit', :id => 100, :format => 'js'
+end
