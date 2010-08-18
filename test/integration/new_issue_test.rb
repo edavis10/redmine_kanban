80b545288c36b62ef3f0f5bc622f3e885fb610eb
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-18 10:33:49 -0700

    [#4270] Added a dummy link to create a new issue to My Kanban Requests.

---
+++ test/integration/new_issue_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+
+class NewIssueTest < ActionController::IntegrationTest
+  def setup
+    configure_plugin
+    setup_kanban_issues
+  end
+
+  context "My Kanban Requests" do
+    setup do
+      @user = User.generate_with_protected!(:login => 'existing', :password => 'existing', :password_confirmation => 'existing')
+    end
+
+    context "for users who don't have permission to open a new issue" do
+      should "not should a new issue link" do
+        login_as
+        visit_my_kanban_requests
+
+        assert_select "a", :text => "New Issue", :count => 0
+      end
+    end
+    
+    context "for users who have permission to open a new issue" do
+      should "show a link to open a new issue" do
+        @project = Project.generate!
+        @role = Role.generate!(:permissions => [:view_issues, :view_kanban, :add_issues])
+        Member.generate!({:principal => @user, :project => @project, :roles => [@role]})
+
+        login_as
+        visit_my_kanban_requests
+
+        assert_select "a", :text => "New issue"
+      end
+      
+    end
+  end
+
+  # Have to simulate JS requests by hitting the same endpoint as jQuery
+  context "Loading the new issue form" do
+    should ''
+  end
+
+  
+end
