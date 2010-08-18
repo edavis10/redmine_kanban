7e723bfb5288b75ac4b3c7be1a374ad6f1dcb7db
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-18 15:29:18 -0700

    [#4270] Added a jquery popup using dialog.

--- assets/javascripts/user_kanban.js
+++ assets/javascripts/user_kanban.js
@@ -5,4 +5,25 @@ jQuery(function($) {
   $('#user_id').change(function() {
     $('form#user_switch').submit();
   });
+
+
+  $('#dialog-window').
+    dialog({
+      autoOpen: false,
+      buttons: {
+        "Cancel": function() {
+          $(this).dialog("close");
+        },
+        "OK": function() {
+          $(this).dialog("close");
+        }
+      }});
+
+
+  $('#new-issue-dialog').click(function() {
+    $('#dialog-window').
+      html(''). // Gets cached
+      html('<h2>Test</h2>').
+      dialog('open');
+  });
 });
