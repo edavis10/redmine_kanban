a866478eee6074d769cda1dc42decd70eb82c542
Eric Davis <edavis@littlestreamsoftware.com>
2010-09-15 14:45:07 -0700

    [#4270] Connect to the Issue JSON api for saving an issue

--- assets/javascripts/user_kanban.js
+++ assets/javascripts/user_kanban.js
@@ -15,9 +15,6 @@ jQuery(function($) {
       buttons: {
         "Cancel": function() {
           $(this).dialog("close");
-        },
-        "OK": function() {
-          $(this).dialog("close");
         }
       }});
 
@@ -29,3 +26,35 @@ jQuery(function($) {
       dialog('open');
   });
 });
+
+function hideAttachmentsForm() {
+  jQuery('#attachments_fields').closest('p').hide();
+}
+
+function registerNewIssueCallbacks() {
+  jQuery('#issue_project_id').change(function() {
+    jQuery('#dialog-window').load('/kanban_issues/new.js', jQuery('#issue-form').serialize())
+  });
+
+  jQuery('#issue-form').submit(function(event) {
+    event.preventDefault();
+    jQuery.ajaxQueue.post(jQuery('#issue-form').attr('action'), {
+      data: jQuery('#issue-form').serialize(),
+      success: function(response) {
+        issue = jQuery.secureEvalJSON(response);
+        var another = confirm("Issue created. Create another?");
+        if (another) {
+          jQuery('#issue-form')[0].reset();
+        } else {
+          jQuery('#dialog-window').dialog("close");
+        }
+      },
+      error: function(response) {
+        jQuery('#issue-form-errors').html('Error saving issue.').show();
+        return false;
+      }
+    });
+
+    return false;
+  });
+}
