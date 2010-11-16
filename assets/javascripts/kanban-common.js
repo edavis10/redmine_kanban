3af7c5c40294306632eae79453ccffaeea0b99ca
Eric Davis <edavis@littlestreamsoftware.com>
2010-11-16 12:41:31 -0800

    [#4162] Refactor: move method to Kanban object

--- assets/javascripts/kanban-common.js
+++ assets/javascripts/kanban-common.js
@@ -7,6 +7,32 @@
 
     },
 
+    // Register callbacks for the New Issue popup form
+    registerNewIssueCallbacks: function() {
+      $('#issue_project_id').change(function() {
+        $('#dialog-window').load('/kanban_issues/new.js', $('#issue-form').serialize())
+      });
+
+      $('#issue_tracker_id').change(function() {
+        $('#dialog-window').load('/kanban_issues/new.js', $('#issue-form').serialize())
+      });
+
+      $('#issue-form').submit(function() {
+        $(this).ajaxSubmit({
+          dataType: 'xml', // TODO: json format would prompt for a file download in the iframe
+          success: function(responseText, statusText, xhr, $form) {
+            $('.flash.notice').html(i18n.kanban_text_issue_created_reload_to_see).show();
+            $('#dialog-window').dialog("close");
+          },
+          error: function(response) {
+            $('#issue-form-errors').html(i18n.kanban_text_error_saving_issue).show();
+          }
+        });
+
+        return false;
+      });
+    },
+
     initilize: function() {
       $(function($) {
         Kanban.initilize_after_dom_loaded();
--- assets/javascripts/user_kanban.js
+++ assets/javascripts/user_kanban.js
@@ -39,31 +39,6 @@ function hideAttachmentsForm() {
   jQuery('#attachments_fields').closest('p').hide();
 }
 
-function registerNewIssueCallbacks() {
-  jQuery('#issue_project_id').change(function() {
-    jQuery('#dialog-window').load('/kanban_issues/new.js', jQuery('#issue-form').serialize())
-  });
-
-  jQuery('#issue_tracker_id').change(function() {
-    jQuery('#dialog-window').load('/kanban_issues/new.js', jQuery('#issue-form').serialize())
-  });
-
-  jQuery('#issue-form').submit(function() {
-    jQuery(this).ajaxSubmit({
-      dataType: 'xml', // TODO: json format would prompt for a file download in the iframe
-      success: function(responseText, statusText, xhr, $form) {
-        jQuery('.flash.notice').html(i18n.kanban_text_issue_created_reload_to_see).show();
-        jQuery('#dialog-window').dialog("close");
-      },
-      error: function(response) {
-        jQuery('#issue-form-errors').html(i18n.kanban_text_error_saving_issue).show();
-      }
-    });
-
-    return false;
-  });
-}
-
 function takeOverWatchLinks(jquerySelector) {
   jQuery(jquerySelector).find('a.icon-fav-off').click(function() {
     watched_issue = true;
